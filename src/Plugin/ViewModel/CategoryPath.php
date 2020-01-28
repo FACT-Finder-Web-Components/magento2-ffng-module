392a1178c126258a4b3f46c9520e130ba8674b09
a.laurowski <ext-a.laurowski@omikron.net>
2020-01-28 09:56:23 +0100

    FFWEB-1521: Fixes/improvements for NG (#1)

---
+++ src/Plugin/ViewModel/CategoryPath.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Omikron\FactfinderNG\Plugin\ViewModel;
+
+use Magento\Catalog\Model\Category;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Registry;
+use Omikron\Factfinder\ViewModel\CategoryPath as ViewModel;
+
+class CategoryPath
+{
+    /** @var Registry */
+    private $registry;
+
+    /** @var string */
+    private $param;
+
+    /** @var string[] */
+    private $initial;
+
+    /** @var ScopeConfigInterface */
+    private $scopeConfig;
+
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        Registry $registry,
+        string $param = 'CategoryPath',
+        array $initial = []
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->param       = $param;
+        $this->registry    = $registry;
+        $this->initial     = $initial;
+    }
+
+    /**
+     * @param ViewModel $subject
+     * @param callable  $proceed
+     * @param mixed     ...$params
+     *
+     * @return string
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function aroundGetValue(ViewModel $subject, callable $proceed, ...$params): string
+    {
+        switch ($this->scopeConfig->getValue('factfinder/advanced/version')) {
+            case 'ng':
+                $categories = $this->getCategoryPath($this->getCurrentCategory());
+                $value      = $this->initial;
+                $value[]    = sprintf('filter=%s', urlencode($this->param . ':' . implode('/', $categories)));
+                return implode(',', $value);
+            default:
+                return $proceed(...$params);
+        }
+    }
+
+    protected function getCategoryPath(Category $category): array
+    {
+        return array_map(function (Category $item): string {
+            return (string) $item->getName();
+        }, $category->getParentCategories());
+    }
+
+    private function getCurrentCategory(): Category
+    {
+        return $this->registry->registry('current_category');
+    }
+}
